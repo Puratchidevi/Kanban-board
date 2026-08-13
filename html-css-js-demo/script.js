6770df554b604d811d1274664cfcfb24519c452b
Puratchidevi <108186018+Puratchidevi@users.noreply.github.com>
2026-08-14 00:17:40 +0530

    Add functions to show message and change color

---
+++ html-css-js-demo/script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+  document.getElementById("message").textContent =
+    "Hello! JavaScript is working.";
+}
+
+function changeColor() {
+  document.getElementById("message").style.color = "blue";
+}
